e6f220493da9c7442b59e923b86ecd13d40ecdcc
fow903 <edwin2delossantos@gmail.com>
2019-05-05 21:06:10 -0400

    nuevos arreglos en compras

---
+++ purchase_stock_availability/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cantidades en Compras",
+    'summary': 'Cantidades',
+
+    'description': """
+    """,
+
+    'author': "Devown",
+    'category': 'Purchase Management',
+    'version': '0.1',
+    'depends': ['purchase','stock'],
+
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+
+    "installable": True,
+}
